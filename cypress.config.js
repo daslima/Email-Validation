c0ee0ce901a92394d43b47e23aaf219fe4b1a589
David <davidlimait@outlook.com>
2022-09-29 23:54:39 -0300

    Install mailosaur

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'http://notes-serverless-app.com',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
 });
